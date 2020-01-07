41141e6adadd5fa4d1836f6f44b489c7cabc4b23
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-01-07 12:25:42 -0700

    fixed bugs in quesiton and answer transitions

--- script.js
+++ script.js
@@ -4,19 +4,15 @@ var seconds = questions.length * 15;
 var timerEl = document.querySelector("#timer");
 var startButton = document.querySelector("#startButton");
 var quizBody = document.querySelector("#quiz");
-var choicesDiv1 = document.querySelector("#choices");
+var questionBody = document.querySelector("#main-question");
+var choicesDiv = document.querySelector("#choices");
 var endScreen = document.querySelector("#end-screen");
 var page1 = document.querySelector(".page1");
 var currentIndex = 0;
-// var answerBtns;
-// var timerId;
-
-// variables to reference DOM elements
-var submitBtn = document.getElementById("submit");
+var scorepage;
 
 //On click: 
-//timer starts and page moves to first question 
-//start 10 second timer
+//timer starts and page moves to first question
 
 function getQuestion(){
 
@@ -25,58 +21,52 @@ function getQuestion(){
   let i = currentIndex;
 
   //Display first question
-  var h3 = document.createElement("h3");
-  h3.textContent = questions[i].title;
-
-  //appends the h3 div to the main div that holds the questions
-  quizBody.append(h3);
+  questionBody.innerText = questions[i].title;
 
   for (j = 0; j < questions[i].choices.length; j++){
-
     //call first set of choices 
     var buttonDiv = document.createElement("div");
     var button = document.createElement("button");
     var resultEl = document.createElement("h4");
     var answerOutput = document.querySelector("#answer-output");
-    var currentQuestion = 0;
-
-    buttonDiv.append(button);
-    button.setAttribute("id", "responses" + j);
-    button.setAttribute("res", questions[i].choices[j]);
-    button.textContent = questions[i].choices[j];
+    console.log("current Index", currentIndex);
     
-    currentQuestion++;
-
-    quizBody.append(buttonDiv);
+    button.setAttribute("id", "responses" + j);
+    button.textContent = questions[i].choices[j];
+    buttonDiv.append(button);
+    choicesDiv.append(buttonDiv);
 
     document.querySelector("#responses" + j).addEventListener("click", function (event) {
-      var selectedAns = event.target.attributes[1].nodeValue;
+      var selectedAns = event.target.innerHTML;
+      console.log(selectedAns);
 
       if (selectedAns === questions[i].answer) {
-
           resultEl.textContent = ("Correct!");
-          answerOutput.innerHTML = "";
+          answerOutput.innerText = "";
           answerOutput.append(resultEl);
-
+          console.log("correct");
       }
       else {
-
           resultEl.textContent = ("Incorrect!");
-          answerOutput.innerHTML = "";
+          answerOutput.innerText = "";
           answerOutput.append(resultEl);
           seconds = seconds - 15
           timer.textContent = "Time: " + seconds;
+          console.log("incorrect");
           verifyTime(); 
     }
 
-  index++;
+  currentIndex++;
+  questionBody.innerHTML ="";
+  choicesDiv.innerHTML="";
 
   // verify if index is < questions.length
-  if (index < questions.length) {
+  if (currentIndex < questions.length) {
       getQuestion();
   }
   else {
       endQuiz();
+      console.log("At the end!!");
   }
 })}
 }
@@ -91,74 +81,39 @@ function verifyTime() {
 }
 
 function endQuiz() {
-  // stop timer
+  quizBody.innerHTML = "";
   clearInterval(timerEl);
-  // show end screen
-  var endScreenEl = document.getElementById("end-screen");
-  endScreenEl.removeAttribute("class");
-  // show final score
-  var finalScoreEl = document.getElementById("final-score");
-  finalScoreEl.textContent = time;
-  // hide questions section
-  scoreContainer.setAttribute("class", "hide");
+  timerEl.remove();
 
+  var scorepage = document.getElementById("scorepage");
 
-  endScreen.innerHTML = "";
-  var total = document.querySelector("#total-score");
+  var finalScoreEl = document.getElementById("total-score");
+  finalScoreEl.textContent = "Final Score: " + seconds;
+  
+  var endNote = document.createElement("h4");
+  endNote.textContent = "Thanks for Playing!"
+  scorepage.prepend(endNote);
+
   var addUser = document.querySelector("#add-user");
-  total.textContet = seconds;
+  var userBtn = document.createElement("button");
+  userBtn.textContent = "Submit Your Score";
+  addUser.append(userBtn);
+  var initials = document.createElement("input");
+  initials.setAttribute("placeholder", "Insert Initials Here");
+  addUser.prepend(initials);
 
-  var resultsArea = document.querySelector("#resultsarea");
-  resultsArea.style.display = "block";
-
-  addUser.addEventListener("click", function () {
+  userBtn.addEventListener("click", function () {
       event.preventDefault()
+
+      localStorage.setItem("User:" + initials, "Score:" + finalScoreEl);
       
-      // the input
-      var initials = document.querySelector("#initials").value;
-      document.querySelector("#initials").value = "";
-
-      // then add to the localstorage
-      // key totalscores [{user:"", score:},]
-
-      if (initials !== "") {
-          var newUser = {
-              user: initials,
-              score: seconds
-          }
-          var totalScores = JSON.parse(localStorage.getItem("totalScores"))
-          console.log(totalScores);
-          if (!totalScores) {
-              totalScores = []
-          }
-          totalScores.push(newUser)
-          localStorage.setItem("totalScores", JSON.stringify(totalScores))
-      }
+      var results = document.querySelector("#results");
+      results.textContent = "Score Board";
+      var initialsList = document.createElement("ul");
+      initialsList.textContent = initials.value + " " + finalScoreEl.textContent;
+      results.append(initialsList);
   })
 }
-
-
-// function saveHighscore() {
-//   // get value of input box
-//   var initials = initialsEl.value.trim();
-//   // make sure value wasn't empty
-//   if (initials !== "") {
-//     // get saved scores from localstorage, or if not any, set to empty array
-//     var highscores =
-//       JSON.parse(window.localStorage.getItem("highscores")) || [];
-//     // format new score object for current user
-//     var newScore = {
-//       score: time,
-//       initials: initials
-//     };
-//     // save to localstorage
-//     highscores.push(newScore);
-//     window.localStorage.setItem("highscores", JSON.stringify(highscores));
-//     // redirect to next page
-//     window.location.href = "highscores.html";
-//   }
-// }
-
 
 startButton.addEventListener("click", function(){
   event.preventDefault();
