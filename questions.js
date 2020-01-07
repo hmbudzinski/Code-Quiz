54757eb660df53c2052091d8b46b0809105a0b6c
Hayley Budzinski <hayleybudzinski@gmail.com>
2020-01-07 12:27:22 -0700

    added more questions

--- questions.js
+++ questions.js
@@ -2,14 +2,29 @@
 //questions for quiz 
 var questions = [
     {
-      question: "1. Commonly used data types DO NOT include:",
+      title: "1. Commonly used data types DO NOT include:",
       choices: ["strings","booleans","alerts","numbers"],
       answer: "alerts"
     },
     {
-      question: "2. The condition in an if / else statement is enclosed within ____.",
+      title: "2. The condition in an if / else statement is enclosed within ____.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
+    },
+    {
+      title: "3. IDs can be called in a css or js file by using the ___ character.",
+      choices: ["#", ".", "$", "@"],
+      answer: "#"
+    },
+    {
+      title: "4. What does an 'event listener' do?",
+      choices: ["listens to what you're saying through your computer's mic", "starts a function after a site event e.g. 'click'", "prompts the user to input", "alerts the user that there was an event on-site"],
+      answer: "starts a function after a site event e.g. 'click'"
+    },
+    {
+      title: "5. What system does Bootstrap offer to help arrange a page",
+      choices: ["html system", "cube system", "grid system", "bracket system"],
+      answer: "grid system"
     },
   ]
 
